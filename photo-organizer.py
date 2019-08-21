8aef74df6d46c35e8ae5686ea723c11c4340b4fe
RAJ66 <vitorbarbosa1998@hotmail.com>
2019-08-21 15:12:14 +0100

    bug none fix

--- photo-organizer.py
+++ photo-organizer.py
@@ -1,11 +1,27 @@
 import os
 from PIL import Image
+from datetime import datetime
 
 #print("Hello World")
 
-def photoShotingData(file):
-  photo = Image.open(file)
-  info = photo._getexif()
-  return os.path.getsize(file)
 
-print(photoShotingData('vitor.jpg'))
+def photoShotingData(file):
+    photo = Image.open(file)
+    info = photo._getexif()
+    if info == None:
+        print('sim')
+    else:
+        print('nao')
+
+    return info
+
+
+print(photoShotingData('test1.jpg'))
+
+'''
+if 36867 in info:
+    date = info[36867]
+    date = datetime.strptime(date,'%Y:%m:%d %H:%M:%S')
+  else:
+    date = datetime.fromtimestamp(os.path.getmtime(file))
+'''
